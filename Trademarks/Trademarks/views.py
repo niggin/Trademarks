cfafd29e7af3a1f53fbba26ffaa00dee3c86b9b0
Niggin <rubik303@mail.ru>
2014-02-25 19:24:36 +0400

    First database and DoubleMetaphon fixes
    +66000 words
    DoubleMetaphon does not cycle anymore (at least on database words)

--- Trademarks/Trademarks/views.py
+++ Trademarks/Trademarks/views.py
@@ -5,14 +5,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from Trademarks.models import Word
 from Trademarks.eng_eng import *
+from multiprocessing import Process, Queue
+import time
+import Trademarks.eng_eng
 
 def home(request):
     context = {}
+    """f = open("c:/users/rubik_000/documents/base.txt")
+    lines = f.read().split('\n')
+    for i in range(len(lines)):
+        to = lines[i].split('@')
+        if len(to) > 1:
+            res = DoubleMetaphon(to[0]).transcription
+            Word.objects.create(word=to[0],meaning=to[1],ipa=res)
+    f.close()"""
+
     return render(request, 'index.html', context)
 
 def search(request):
     p = DoubleMetaphon(request.POST['findme']).transcription 
-    te = list(Word.objects.filter(ipa__startswith=p))
+    te = list(Word.objects.filter(ipa__contains = p))
     p = request.POST['findme'] + ': [' + p + ']'
     context = {'text' : p, 'array' : te}
     return render(request, 'search.html', context)
